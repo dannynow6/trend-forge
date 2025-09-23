79ada56f54868df69825e1cded8676feab88cac0
Dan Garro <dobetterwithdan@gmail.com>
2025-09-23 17:32:56 -0400

    added animations to Logo component in /components/home

--- src/components/home/Logo.tsx
+++ src/components/home/Logo.tsx
@@ -1,11 +1,17 @@
+"use client";
+import { motion } from "motion/react";
 import LogoImg from "../../../public/logo.png";
 import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="flex items-center justify-center p-4 rounded-full h-32 w-32 bg-gradient-to-br from-sky-100 to-blue-100 shadow-lg">
+    <motion.div
+      whileHover={{ scale: 1.05, rotate: 5 }}
+      transition={{ type: "spring", stiffness: 300, damping: 20 }}
+      className="flex items-center justify-center p-2 rounded-full h-32 w-32 bg-gradient-to-br from-sky-100 to-blue-100 shadow-lg"
+    >
       <Image src={LogoImg} alt="Logo" className="w-24 h-24" priority />
-    </div>
+    </motion.div>
   );
 };
 
